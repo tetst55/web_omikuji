801310bc1c86bbadf4f55a2a4b42bc2842d7cbf6
tetst55 <ymnhkwyn5m@privaterelay.appleid.com>
2026-06-08 19:34:00 +0900

    Update tkinter.omikuji.py

--- tkinter.omikuji.py
+++ tkinter.omikuji.py
@@ -9,13 +9,13 @@ dir_path = os.path.dirname(__file__)
 
 # ボタンが押されたらおみくじを引く
 if st.button("占う", type="primary"):
-kujis = ["daikichi.png", "chukichi.png", "kichi.png", "kyo.png"]
-result_img = random.choice(kujis)
+    kujis = ["daikichi.png", "chukichi.png", "kichi.png", "kyo.png"]
+    result_img = random.choice(kujis)
 
 # 画像の場所をドッキング
-img_path = os.path.join(dir_path, result_img)
-st.image(img_path, width=300)
+    img_path = os.path.join(dir_path, result_img)
+    st.image(img_path, width=300)
 else:
 # まだボタンを押していない時は empty.png を出す
-empty_path = os.path.join(dir_path, "empty.png")
-st.image(empty_path, width=300)
+    empty_path = os.path.join(dir_path, "empty.png")
+    st.image(empty_path, width=300)
